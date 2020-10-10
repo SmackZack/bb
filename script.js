86c82c0b5fe4e5d6093b7e2cde44182407677388
SmackZack <ashishone9781@gmail.com>
2020-10-10 21:18:43 +0530

    added the covid data

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var form = new FormData();
 form.append("category", "movies");
-form.append("language", "kannada");
+form.append("language", "hindi");
 form.append("genre", "all");
-form.append("sort ", "voting");
+form.append("sort ", "");
 
 var settings = {
   url: "https://hoblist.com/movieList",
@@ -19,61 +19,117 @@ var settings = {
 };
 
 $.ajax(settings).done(function (response) {
-  console.log(JSON.parse(response).result[0]);
-  var list =""
-  for(var i=0; i< JSON.parse(response).result.length; i++)
-  {
-      list +=
-        `<div class="col-4 d-flex justify-content-center mb-4">
-        <div class="card shadow-lg" style="width: 18rem;">
-  <img src="` +
-        JSON.parse(response).result[i].poster +
-        `" class="card-img-top" height="300px" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">` +
-        JSON.parse(response).result[i].title +
-        `</h5>
-    <p class="card-text">Genre:` +
-        JSON.parse(response).result[i].genre +
-        `</p>
-   <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#dsd` +
-        JSON.parse(response).result[i]._id +
-        `">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="dsd` +
-        JSON.parse(response).result[i]._id +
-        `" tabindex="-1" aria-labelledby="dsd` +
-        JSON.parse(response).result[i]._id +
-        `Label" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="dsd` +
-        JSON.parse(response).result[i]._id +
-        `Label">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
+  var list = "";
+  for (var i = 0; i < JSON.parse(response).result.length; i++) {
+    list +=
+      ` <div class="col-lg-6 py-3">
+    <div class="row py-3 border border-dark no-gutters shadow-lg rounded-lg bg-light">
+      <div class="col-2 text-center">
+        <button class="btn btn p-0">
+          <i class="fas fa-3x fa-caret-up"></i>
+        </button>
+        <div class="h2 mb-0">0</div>
+        <button class="btn btn p-0">
+          <i class="fas fa-rotate-180 fa-3x fa-caret-up"></i>
         </button>
       </div>
-      <div class="modal-body">
-       ` +
-        JSON.stringify(JSON.parse(response).result[i]) +
-        `
+      <div class="col-10">
+        <div class="media">
+          <img
+            src="` +
+      JSON.parse(response).result[i].poster +
+      `"
+            height="150px"
+            width="100px"
+            class="mr-3 rounded-lg shadow-lg border border-dark"
+            alt="..."
+          />
+          <div class="media-body">
+            <h4 class="mt-0">` +
+      JSON.parse(response).result[i].title +
+      `</h4>
+            <p class="my-0">
+              <small
+                ><span class="text-secondary">Genre:</span>` +
+      JSON.parse(response).result[i].genre +
+      `</small
+              ><small
+                ><br /><span class="text-secondary">Director:</span>` +
+      JSON.parse(response).result[i].director +
+      `</small
+              ><br /><small style="height: 100px"
+                ><span class="text-secondary">Starring:</span>` +
+      JSON.parse(response).result[i].stars +
+      `</small
+              >
+            </p>
+            <p class="my-0">
+              <small
+                >` +
+      JSON.parse(response).result[i].runTime +
+      ` | ` +
+      JSON.parse(response).result[i].language +
+      ` | ` +
+      JSON.parse(response).result[i].releasedDate +
+      `</small
+              >
+            </p>
+            <p class="my-0 text-primary">
+              <small
+                >` +
+      JSON.parse(response).result[i].pageViews +
+      ` | Voted by ` +
+      JSON.parse(response).result[i].totalVoted +
+      ` people</small
+              >
+            </p>
+          </div>
+        </div>
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+      <div class="col-12 d-flex justify-content-center">
+        <button class="btn btn-primary m-2 px-5"><div class="px-5">Watch Trailer</div></button>
       </div>
     </div>
-  </div>
-</div>
-  </div>
-</div>
-      </div>`;
-      console.log(JSON.parse(response).result[0].poster);
+  </div>`;
   }
-  document.getElementById("movieList").innerHTML= list
+  document.getElementById("movieList").innerHTML = list;
+});
+
+//covid data
+
+var settings = {
+  url: "https://api.covid19api.com/summary",
+  method: "GET",
+  timeout: 0,
+};
+
+$.ajax(settings).done(function (response) {
+  var list = "";
+  for (var i = 0; i < response.Countries.length; i++) {
+    list +=
+      ` <tr>
+              <td>` +
+      response.Countries[i].Country +i+
+      `</td>
+              <td>` +
+      response.Countries[i].NewConfirmed +
+      `</td>
+              <td>` +
+      response.Countries[i].NewDeaths +
+      `</td>
+              <td>` +
+      response.Countries[i].NewRecovered +
+      `</td>
+              <td class="text-primary">` +
+      response.Countries[i].TotalConfirmed +
+      `</td>
+              <td class="text-danger">` +
+      response.Countries[i].TotalDeaths +
+      `</td>
+              <td class="text-success">` +
+      response.Countries[i].TotalRecovered +
+      `</td>
+            </tr>`;
+    document.getElementById("countryList").innerHTML = list;
+  }
 });
